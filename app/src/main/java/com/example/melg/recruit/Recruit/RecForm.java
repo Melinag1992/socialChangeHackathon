854970aeee4ca9b5e584090635f9b36299f307f5
joannesong <joannexsong@gmail.com>
2018-08-11 18:12:08 -0400

    added UserRegisterActivity

--- app/src/main/java/com/example/melg/recruit/Recruit/RecForm.java
+++ app/src/main/java/com/example/melg/recruit/Recruit/RecForm.java
@@ -11,7 +11,7 @@ public class RecForm extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.form_rec);
+        //setContentView(R.layout.form_rec);
 
 
     }
