14ea2c060a2f5677d7458b3f23ab0edf23f9a862
Melina Gonzalez <melinag1992@gmail.com>
2018-08-11 15:54:53 -0400

    initial commit

---
+++ app/src/main/java/com/example/melg/recruit/Recruit/RecForm.java
@@ -0,0 +1,18 @@
+package com.example.melg.recruit.Recruit;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.melg.recruit.R;
+
+public class RecForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.form_rec);
+
+
+    }
+}
