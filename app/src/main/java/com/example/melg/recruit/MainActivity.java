d8e25d0c1aa138b418e367ea79fd0e8532a8e995
7sco <xaviandrade14@gmail.com>
2018-08-12 09:37:26 -0400

    views connected

--- app/src/main/java/com/example/melg/recruit/MainActivity.java
+++ app/src/main/java/com/example/melg/recruit/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.melg.recruit;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
-import android.widget.TextView;
 
+import com.example.melg.recruit.Recruit.RecForm;
 import com.example.melg.recruit.Recruit.discover.view.DiscoverActivity;
 
 import butterknife.BindView;
@@ -16,9 +16,9 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.email_edit_text_register)
-    TextView email;
+    TextInputLayout email;
     @BindView(R.id.password_edit_text_register)
-    TextView password;
+    TextInputLayout password;
     @BindView(R.id.sign_in_button)
     Button signInButton;
     @BindView(R.id.register_button)
@@ -30,15 +30,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         ButterKnife.bind(this);
-
     }
 
-    @OnClick(R.id.button)
+    @OnClick(R.id.sign_in_button)
     public void onViewClicked() {
-
-        Intent view = new Intent(this, DiscoverActivity.class);
+        Intent view;
+//        if(email.getEditText().equals("recruiter@mail.com")){
+//            view = new Intent(this, RecForm.class);
+//        }
+//        else {
+//            //User profile
+//            view = new Intent(this, DiscoverActivity.class);
+//        }
+        view = new Intent(this, RecForm.class);
         startActivity(view);
+        finish();
+
     }
+
+//    @OnClick(R.id.button)
+//    public void onViewClicked() {
+//        Intent view = new Intent(this, DiscoverActivity.class);
+//        startActivity(view);
+//    }
 }
--- app/src/main/java/com/example/melg/recruit/Recruit/RecForm.java
+++ app/src/main/java/com/example/melg/recruit/Recruit/RecForm.java
@@ -1,22 +1,43 @@
 package com.example.melg.recruit.Recruit;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.melg.recruit.R;
+import com.example.melg.recruit.Recruit.discover.view.DiscoverActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class RecForm extends AppCompatActivity {
+
+    @BindView(R.id.btnFind)
+    Button btnFind;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.form_rec);
-
+        setContentView(R.layout.form_rec);
+        ButterKnife.bind(this);
         getSupportActionBar().setTitle("Developer Request");
 
 
+    }
 
+    @OnClick(R.id.btnFind)
+    public void onViewClicked() {
+        Intent view = new Intent(this, DiscoverActivity.class);
+        startActivity(view);
+    }
 
+    @Override
+    public void onBackPressed() {
+        Toast.makeText(this, "You Are About to exit the app", Toast.LENGTH_LONG).show();
     }
 }
